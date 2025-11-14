abd2ed74c371a069097bc668280ca9db63dadd71
Lokesh-102214 <quan655737@gmail.com>
2025-11-14 19:30:24 +0530

    Initial commit: Flutter Hand-e-Kraft app

---
+++ hand_e_kraft_appworking/README.md
@@ -0,0 +1,3 @@
+# hand_e_kraft_app
+
+A new Flutter project.
---
+++ hand_e_kraft_appworking/android/app/src/main/kotlin/com/example/hand_e_kraft_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hand_e_kraft_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
